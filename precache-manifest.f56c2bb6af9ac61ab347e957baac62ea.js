93f5ef6b2e47ba3f0ecf9fa777b4c9be80c77ff6
Bannywalia <Banny.walia07@gmail.com>
2023-01-29 14:01:43 -0500

    Updates

---
+++ precache-manifest.f56c2bb6af9ac61ab347e957baac62ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fe04e8ce2db97ddc81f816f226db13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5b589e3117e3ceca47",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "782bae897e017be9524c",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "8b5b589e3117e3ceca47",
+    "url": "/static/js/2.e5925cd3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e5925cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782bae897e017be9524c",
+    "url": "/static/js/main.d2cbf82e.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
